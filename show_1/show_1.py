23d523dca2fa883cc83cda791d7560a26f711ea0
佳炜 <394237449@qq.com>
2018-02-01 18:29:02 +0800

    add readme & add commit

--- show_1/show_1.py
+++ show_1/show_1.py
@@ -1,10 +1,14 @@
 from PIL import Image, ImageDraw, ImageFont
 # get an image
 base = Image.open('computer_life.png')
-
 draw = ImageDraw.Draw(base)
 
+# color
 fillcolor = "#00ff00"
 width,height = base.size
+
+# draw
 draw.text((450,40),'1',fill=fillcolor)
+
+# save
 base.save('result.png')
